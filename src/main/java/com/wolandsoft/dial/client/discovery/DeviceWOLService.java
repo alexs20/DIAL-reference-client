922454818b39e7f35b9e22ec556a6479dcef8c02
Alexander Shulgin <alexs20@gmail.com>
2019-11-19 17:54:44 -0700

    wol draft 1

--- src/main/java/com/wolandsoft/dial/client/discovery/DeviceWOLService.java
+++ src/main/java/com/wolandsoft/dial/client/discovery/DeviceWOLService.java
@@ -1,5 +1,6 @@
 package com.wolandsoft.dial.client.discovery;
 
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
@@ -7,15 +8,20 @@ import java.net.InterfaceAddress;
 import java.net.NetworkInterface;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Enumeration;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import com.wolandsoft.dial.client.InternalException;
 
 public class DeviceWOLService implements SSDPMSearchListener {
-	public static final int PORT = 9;
+	private static final int PORT = 9;
+	private Map<String, Long> usns = Collections.synchronizedMap(new HashMap<>());
 
 	private final ScheduledExecutorService scheduler;
 
@@ -23,19 +29,7 @@ public class DeviceWOLService implements SSDPMSearchListener {
 		this.scheduler = Executors.newScheduledThreadPool(1);
 	}
 
-	public void wakeup(String mac, String usn) {
-		wakeup(mac, null, usn, null);
-	}
-
-	public void wakeup(String mac, String usn, DeviceWOLCallback callback) {
-		wakeup(mac, null, usn, callback);
-	}
-
-	public void wakeup(String mac, String broadcastAddress, String usn) {
-		wakeup(mac, broadcastAddress, usn, null);
-	}
-
-	public void wakeup(String mac, String broadcastAddress, String usn, DeviceWOLCallback callback) {
+	public void wakeup(String mac, int timeout, String broadcastAddress, String usn, DeviceWOLCallback callback) {
 		List<InetAddress> bcAddress;
 		if (broadcastAddress != null) {
 			bcAddress = new ArrayList<>();
@@ -50,10 +44,63 @@ public class DeviceWOLService implements SSDPMSearchListener {
 				throw new InternalException("Cannot determine local broadcast group(s)");
 			}
 		}
-		byte [] macBytes = getMacBytes(mac);
+		byte[] macBytes = getMacBytes(mac);
 		if (macBytes == null) {
 			throw new InternalException("Invalid MAC address");
 		}
+		byte[] bytes = new byte[6 + 16 * macBytes.length];
+		for (int i = 0; i < 6; i++) {
+			bytes[i] = (byte) 0xff;
+		}
+		for (int i = 6; i < bytes.length; i += macBytes.length) {
+			System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+		}
+		List<DatagramPacket> magicPackets = new ArrayList<>();
+		for (InetAddress bcAddr : bcAddress) {
+			DatagramPacket packet = new DatagramPacket(bytes, bytes.length, bcAddr, PORT);
+			magicPackets.add(packet);
+		}
+		usns.put(usn, System.currentTimeMillis());
+		scheduler.schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				if (!usns.containsKey(usn)) {
+					if (callback != null) {
+						scheduler.submit(new Runnable() {
+							@Override
+							public void run() {
+								callback.onDeviceWOLSucceed();
+							}
+						});
+					}
+				} else {
+					Long startTime = usns.get(usn);
+					if (startTime.longValue() + TimeUnit.SECONDS.toMillis(timeout) < System.currentTimeMillis()) {
+						usns.remove(usn);
+						if (callback != null) {
+							scheduler.submit(new Runnable() {
+								@Override
+								public void run() {
+									callback.onDeviceWOLFail();
+								}
+							});
+						}
+						return;
+					} else {
+						try (DatagramSocket socket = new DatagramSocket()) {
+							for (DatagramPacket packet : magicPackets) {
+								socket.send(packet);
+							}
+						} catch (IOException e) {
+
+						}
+						scheduler.schedule(this, 50, TimeUnit.MILLISECONDS);
+					}
+				}
+
+			}
+		}, 50, TimeUnit.MILLISECONDS);
 	}
 
 	private List<InetAddress> getLocalBroadcastAddresses() {
@@ -77,42 +124,6 @@ public class DeviceWOLService implements SSDPMSearchListener {
 		return ret;
 	}
 
-	public static void main(String[] args) {
-
-		if (args.length != 2) {
-			System.out.println("Usage: java WakeOnLan <broadcast-ip> <mac-address>");
-			System.out.println("Example: java WakeOnLan 192.168.0.255 00:0D:61:08:22:4A");
-			System.out.println("Example: java WakeOnLan 192.168.0.255 00-0D-61-08-22-4A");
-			System.exit(1);
-		}
-
-		String ipStr = args[0];
-		String macStr = args[1];
-
-		try {
-			byte[] macBytes = getMacBytes(macStr);
-			byte[] bytes = new byte[6 + 16 * macBytes.length];
-			for (int i = 0; i < 6; i++) {
-				bytes[i] = (byte) 0xff;
-			}
-			for (int i = 6; i < bytes.length; i += macBytes.length) {
-				System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
-			}
-
-			InetAddress address = InetAddress.getByName(ipStr);
-			DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
-			DatagramSocket socket = new DatagramSocket();
-			socket.send(packet);
-			socket.close();
-
-			System.out.println("Wake-on-LAN packet sent.");
-		} catch (Exception e) {
-			System.out.println("Failed to send Wake-on-LAN packet: + e");
-			System.exit(1);
-		}
-
-	}
-
 	private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
 		byte[] bytes = new byte[6];
 		String[] hex = macStr.split("(\\:|\\-)");
@@ -131,7 +142,7 @@ public class DeviceWOLService implements SSDPMSearchListener {
 
 	@Override
 	public void onDeviceDiscovery(SSDPMSearchResponce message) {
-		// TODO Auto-generated method stub
+		usns.remove(message.getUsn());
 
 	}
 }
